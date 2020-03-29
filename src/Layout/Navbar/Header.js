cb12abe4dd2496ca71869e763d08d8f07ee5df80
LFRusso <lf.santos@usp.br>
2020-03-29 00:04:23 -0300

    atualiando

--- src/Layout/Navbar/Header.js
+++ src/Layout/Navbar/Header.js
@@ -9,11 +9,9 @@ const Header = () => {
     const btnClicked = () => {
         if(btnColapsed){
             document.getElementById("sidenav").style.width = "200px";
-            document.getElementById("main").style.marginLeft = "200px";
             document.getElementById("colapse-btn").style.marginLeft = "200px";
         } else {
             document.getElementById("sidenav").style.width = "0"; 
-            document.getElementById("main").style.marginLeft = "10px";
             document.getElementById("colapse-btn").style.marginLeft = "0";
         }
         setBtn(!btnColapsed);
@@ -22,12 +20,10 @@ const Header = () => {
     return (
 
         <div className="">
-
-            <button className="btn" id="colapse-btn"  onClick={ btnClicked }>
-                <FaBars color="#022B3A" size={30} /> 
-            </button>
-
             <Navbar />
+            <button className="btn" id="colapse-btn"  onClick={ btnClicked }>
+                <FaBars color="#efefef" size={30} /> 
+            </button>
         </div>
     );
 }
