15951e6291ac303593036d63052dabdb0140912e
LFRusso <lf.santos@usp.br>
2020-03-29 21:30:52 -0300

    atualizando

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from  'react-router-dom';
 
 import Navbar from './Layout/Navbar/Navbar'
-import Footer from './Layout/Footer/Footer'
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -16,7 +15,7 @@ const Routes = () => {
             <Navbar />
 
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route path="/" component={Home} />
                 <Route path="/about" component={About} />
                 <Route path="/projects" component={Projects} />
                 <Route path="/notes" component={Notes} />
